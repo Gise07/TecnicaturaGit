df6cbf11783b0e64d44c600602eb19ca5756ec68
Gisela Vizcaino <giselavizcaino5@gmail.com>
2022-09-30 20:18:19 -0300

    Paso de argumentos en Java y Operaciones
    Clase en Java sobre Argumentos y Operador this

--- Java/Leccion2/EjercicioCiclos01/src/Ciclos01/Ejercicio_01.java
+++ Java/Leccion2/EjercicioCiclos01/src/Ciclos01/Ejercicio_01.java
@@ -12,7 +12,7 @@ public class Ejercicio_01 {
            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
            while(numero >= 0){
                cuadrado = (int)Math.pow(numero, 2);
-               System.out.println("El numero "+numero+"elevado al cuadrado es: "+cuadrado);
+               System.out.println("El numero "+numero+" elevado al cuadrado es: "+cuadrado);
                numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro numero: "));
            }
            System.out.println("El programa ha finalizado por numero negativo");  
